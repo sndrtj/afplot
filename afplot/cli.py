1cb65e4fc4d2ea8d02ecebeb1808d64e4120dfe9
Sander Bollen <a.h.b.bollen@lumc.nl>
2017-06-01 16:21:24 +0200

    shared options

--- afplot/cli.py
+++ afplot/cli.py
@@ -6,7 +6,96 @@ afplot.cli
 :license: MIT
 """
 
+import re
+
 import click
+
+from .utils import Region
+
+
+def validate_region_str(ctx, param, value):
+    region_re = re.compile('^([\w\d]+):(\d+)-(\d+)$')
+    match = region_re.match(value)
+    if match is not None:
+        return Region(match.group(1),
+                      int(match.group(2)),
+                      int(match.group(3)))
+    else:
+        raise click.BadParameter('{0} is not a '
+                                 'valid region string'.format(value))
+
+
+shared_options_regions = [
+    click.option("--vcf",
+                 "-v",
+                 type=click.Path(exists=True),
+                 required=True,
+                 help="Path to input VCF file"),
+    click.option("--output-dir",
+                 "-o",
+                 type=click.Path(exists=True),
+                 required=True,
+                 help="Path to output directory"),
+    click.option("--region-file",
+                 "-L",
+                 type=click.Path(exists=True),
+                 help="Path to region file"),
+    click.option("--region",
+                 "-R",
+                 callback=validate_region_str,
+                 help="Region string. Must be of format <contig:start-end>"),
+    click.option("--margin",
+                 "-m",
+                 type=int,
+                 help="Margin around regions to plot",
+                 default=0)
+]
+
+
+shared_options_genome = [
+    click.option("--vcf",
+                 "-v",
+                 type=click.Path(exists=True),
+                 required=True,
+                 multiple=True,
+                 help="Path(s) to input VCF file(s)"),
+    click.option("--label",
+                 "-l",
+                 type=str,
+                 required=True,
+                 multiple=True,
+                 help="Label(s) to VCF file(s)"),
+    click.option("--sample",
+                 "-s",
+                 type=str,
+                 multiple=True,
+                 help="Sample name(s) of VCF file(s). "
+                      "If not given, will use fist sample in each VCF File"),
+    click.option("--exclude-pattern",
+                 "-e",
+                 type=str,
+                 multiple=True,
+                 help="Regex pattern(s) to exclude from contig list"),
+    click.option("--output",
+                 "-o",
+                 type=click.Path(exists=False),
+                 required=True,
+                 help="Path to output file")
+]
+
+
+def generic_option(options):
+    """
+    Decorator to add generic options to Click CLI's
+    The group parent should NOT be decorated with this decorator
+    :param options: list of click.option
+    :return: decorated function
+    """
+    def __generic_option(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return __generic_option
 
 
 @click.group(short_help="Whole-genome plots")
@@ -15,18 +104,21 @@ def cli_whole_genome(**kwargs):
     pass
 
 
+@generic_option(shared_options_genome)
 @click.command(short_help="Whole-genome histogram")
 def whole_genome_histogram(**kwargs):
     """Create histograms over every chromosome."""
     pass
 
 
+@generic_option(shared_options_genome)
 @click.command(short_help="Whole-genome scatter plot")
 def whole_genome_scatter(**kwargs):
     """Create scatter plot of allele frequencies over every chromosome."""
     pass
 
 
+@generic_option(shared_options_genome)
 @click.command(short_help="Whole-genome distance plot")
 def whole_genome_distance(**kwargs):
     """Create scatter plot distance to theoretical AF over very chromosome."""
@@ -39,18 +131,21 @@ def cli_regions(**kwargs):
     pass
 
 
+@generic_option(shared_options_regions)
 @click.command(short_help="Region histogram")
 def region_histogram(**kwargs):
     """Create histograms of allele frequencies over every region."""
     pass
 
 
+@generic_option(shared_options_regions)
 @click.command(short_help="Region scatter plot")
 def region_scatter(**kwargs):
     """Create scatter plot of allele frequencies over every region."""
     pass
 
 
+@generic_option(shared_options_regions)
 @click.command(short_help="Region distance plot")
 def region_distance(**kwargs):
     """Create scatter plot ofdistance to theoretical AF over every region."""
@@ -60,8 +155,8 @@ def region_distance(**kwargs):
 @click.group()
 def cli():
     """
-    Plot allele frequencies in VCF files. 
-    
+    Plot allele frequencies in VCF files.
+
     \b
     Two basic modes exist:
       - regions: Plot histogram, scatter or distance plots per
@@ -83,9 +178,6 @@ def main():
     cli.add_command(cli_whole_genome, "whole-genome")
     cli()
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
---
+++ afplot/utils.py
@@ -0,0 +1,11 @@
+"""
+afplot.utils
+~~~~~~~~~~~~
+:copyright: (c) 2017 Sander Bollen
+:copyright: (c) 2017 Leiden University Medical Center
+:license: MIT
+"""
+
+from collections import namedtuple
+
+Region = namedtuple("Region", ["chr", "start", "end"])
